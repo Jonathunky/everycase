a1cc286887446225b3d555f812ccfcb86285ccd3
Jonathunky <12529409+JonathanX64@users.noreply.github.com>
2023-04-07 14:51:52 +0200

    Nothing will change though, eh?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const withNextra = require("nextra")({
-	theme: "nextra-theme-docs",
-	themeConfig: "./theme.config.tsx",
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
 });
 
-module.exports = withNextra();
+const nextConfig = {
+  images: {
+    domains: ["applecase.wiki/assets"],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 768],
+  },
+};
+
+module.exports = withNextra(nextConfig);
