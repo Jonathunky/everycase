4a68b262c7aeb0712208a4dfa0cdd51b00424a68
Jonathunky <12529409+JonathanX64@users.noreply.github.com>
2023-10-07 01:48:23 +0200

    Many PWA improvements

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 const logo = (
   <>
     <img
-      src="/icon-512x512.png"
+      src="/icons/icon-512x512.png"
       alt="EveryCase Logo"
       className="logo"
     />
@@ -71,35 +71,35 @@ const config: DocsThemeConfig = {
 
     return (
       <>
-        <link rel="icon" href="/icon.png" type="image/png" sizes="64x64" />
-        <meta name="robots" content="noindex" />
-        <meta name="og:title" content={title ? title : "EveryCase"} />
+        {/* Basic Information */}
+        <link rel="icon" href="/icons/android-chrome-512x512.png" type="image/png" sizes="512x512" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="application-name" content="next PWA demo" />
+        <meta name="theme-color" content="#BF4F95" />
+        <meta name="application-name" content="EveryCase" />
+        <meta name="description" content="Library of Apple cases for iPhone, iPad, and Mac." />
+
+        {/* OpenGraph Tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title ? title : "EveryCase"} />
+        <meta property="og:description" content="Library of Apple cases for iPhone, iPad, and Mac." />
+        <meta property="og:image" content="/icons/icon.png" />
+
+        {/* Apple Specific Tags */}
+        <meta name="apple-mobile-web-app-title" content="EveryCase" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PWA App" />
-        <meta name="description" content="Best PWA App in the world" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="none" />{" "}
-        {/* learn more  https://blog.giantgeek.com/?p=1418 */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#bf4f95" />
+
+        {/* Microsoft Specific Tags */}
+        <meta name="msapplication-config" content="none" />
         <meta name="msapplication-TileColor" content="#FFE0F5" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#BF4F95" />
-        <meta
-          property="og:description"
-          content="The website about Apple cases"
-        />
-        <style>
-          {`
-            @font-face {
-              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-              font-style: normal;
-              font-weight: 400;
-            }
-          `}
-        </style>
+
+        {/* Miscellaneous */}
+        <meta name="robots" content="noindex" /> {/* until it's ready */}
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
       </>
     );
   },
