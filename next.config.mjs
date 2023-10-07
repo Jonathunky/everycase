236b1c246dab75df7137581c6fd4627d6d1bb176
Jonathunky <12529409+JonathanX64@users.noreply.github.com>
2023-10-07 03:01:09 +0200

    Offline capabilities in PWA

--- next.config.mjs
+++ next.config.mjs
@@ -57,6 +57,18 @@ const pwaConfig = {
   pwa: {
     dest: "public",
     disable: process.env.NODE_ENV === "development",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "offlineCache",
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
   },
 };
 
