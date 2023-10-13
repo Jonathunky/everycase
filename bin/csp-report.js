bf44761ab6f86a1f60b1fe20d093302f50c14814
Jonathunky <12529409+Jonathunky@users.noreply.github.com>
2023-10-13 23:04:05 +0200

    CSP experiments

---
+++ bin/csp-report.js
@@ -0,0 +1,10 @@
+export default function (req, res) {
+  const report = req.body;
+
+  // You can log, store, or process the CSP report as needed.
+  // For this example, we'll just log it.
+  console.log("CSP Report:", report);
+
+  // Respond with a 204 No Content status.
+  res.status(204).end();
+}
