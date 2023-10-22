91d7e01ed00a7a8481188b367b76d0af747ad3ae
Jonathunky <12529409+Jonathunky@users.noreply.github.com>
2023-10-22 15:37:38 +0200

    Introduced Tailwind CSS, fixed anal ytics

--- components/LightboxComponent.jsx
+++ components/LightboxComponent.jsx
@@ -4,9 +4,9 @@ import { SlideshowLightbox } from 'lightbox.js-react';
 const LightboxComponent = ({ src, alt }) => {
     return (
         <SlideshowLightbox className='container grid grid-cols-3 gap-2 mx-auto' showThumbnails={true}>
-            <img className='w-full rounded' src='https://cloudfront.everycase.org' />
-            <img className='w-full rounded' src='https://cloudfront.everycase.org' />
-            <img className='w-full rounded' src='https://cloudfront.everycase.org/everypreview/MF041.webp' />
+            <img className='w-full rounded' src='https://cloudfront.everycase.org/everysource/MF041.webp' />
+            <img className='w-full rounded' src='https://cloudfront.everycase.org/everysource/MF041.webp' />
+            <img className='w-full rounded' src='https://cloudfront.everycase.org/everysource/MF041.webp' />
 
         </SlideshowLightbox>
     );
